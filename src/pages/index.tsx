27d96d6dcb74018aaa814a147366ca715c10c50b
Carlos <36555654+lospoy@users.noreply.github.com>
2023-06-08 12:28:48 -0400

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,14 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import Link from "next/link";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
-
+  // In a pro app we could use Zod and React Hook form to invalidate input in-client
+  // Also we'd use React Hook form to manage the input state
+  // Here we'll probably get a 'sticky key' behavior (since we're re-rendering on key press)
   const [input, setInput] = useState("");
 
   const ctx = api.useContext();
@@ -86,10 +89,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span> ·{" "}
-          <span className="font-thin">{`${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{`· ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
